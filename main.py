9d02e42d2e0d559a084b7055644bcc25fcec4c4e
mattcmerritt <mattcmerritt@gmail.com>
2023-04-01 12:28:17 -0400

    added simple database connection functionality

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello!")
+import pymysql
+
+def connect_to_server(user, password) -> pymysql.connections.Connection:
+    return pymysql.connect(db='schedules', user=user, passwd=password, host='localhost', port=3306)
+
+
+if (__name__ == "__main__"):
+    connection = connect_to_server('', '')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM UserSchedules')
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    connection.close()
